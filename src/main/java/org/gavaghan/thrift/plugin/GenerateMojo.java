3263a33fe95ca08cb0707916bc30a924d5cbac49
Mike Gavaghan <mike@gavaghan.org>
2017-09-12 20:25:04 -0700

    Set default phase

--- src/main/java/org/gavaghan/thrift/plugin/GenerateMojo.java
+++ src/main/java/org/gavaghan/thrift/plugin/GenerateMojo.java
@@ -10,6 +10,7 @@ import org.apache.maven.model.Build;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
@@ -19,7 +20,7 @@ import org.apache.maven.project.MavenProject;
  * 
  * @author <a href="mailto:mike@gavaghan.org">Mike Gavaghan</a>
  */
-@Mojo(name = "generate")
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GenerateMojo extends AbstractMojo
 {
 	/** Build subdirectory for generated files. */
